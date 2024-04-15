14b1741a405c7608992cb0b33f13ab21ae83347e
tedschu <ted.schuster@gmail.com>
2024-04-15 12:52:28 -0500

    added files

--- app.js
+++ app.js
@@ -3,12 +3,14 @@
 const taxRate = 0.07;
 const tip = 0.2;
 
+//user input
 let bill = prompt("How much was your bill?");
 bill = Number(bill);
 
+//computes the input + tax + tip
 let finalBill = bill + (bill * taxRate) + (bill * tip);
 
-
+//outputs the final bill
 console.log(finalBill)
 
 
