0e99b9272af45c4972dd15c264593d5b76b3243b
tedschu <ted.schuster@gmail.com>
2024-04-15 12:53:10 -0500

    upload

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
+//creating variables, some referencing each other to oupput combination numbers
+const num1 = 5 * 2;  
+const num2 = num1 + 30;  //answer is 40
+const num3 = num2 - 1;   //answer is 39
 
-//creating variables
-const taxRate = 0.07;
-const tip = 0.2;
+const finalOutput = num1 + " - " + num2 + " - " + num3;
 
-//user input
-let bill = prompt("How much was your bill?");
-bill = Number(bill);
 
-//computes the input + tax + tip
-let finalBill = bill + (bill * taxRate) + (bill * tip);
 
-//outputs the final bill
-console.log(finalBill)
+
+
+
+
+
+
+// //creating variables
+// const taxRate = 0.07;
+// const tip = 0.2;
+
+// //user input
+// let bill = prompt("How much was your bill?");
+// bill = Number(bill);
+
+// //computes the input + tax + tip
+// let finalBill = bill + (bill * taxRate) + (bill * tip);
+
+// //outputs the final bill
+// console.log(finalBill)
 
 
 
